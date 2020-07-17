aafd9c7642c2e6bc3a360f474cf783bacaa1ddb0
Maxim <49313691+MaxZayats@users.noreply.github.com>
2020-07-17 16:23:03 +0300

    Update modify_template.py

--- modules/modify_template.py
+++ modules/modify_template.py
@@ -4,9 +4,9 @@ def modify(user_template_name, base_template_name, main_template_name):
     A script and a custom template are added to the "main_template_name" template.
 
     """
-    user_template = open('server/templates/'+user_template_name, 'r')
-    base_template = open('server/templates/'+base_template_name, 'r')
-    main_template = open('server/templates/'+main_template_name, 'w')
+    user_template = open('templates/'+user_template_name, 'r')
+    base_template = open('templates/'+base_template_name, 'r')
+    main_template = open('templates/'+main_template_name, 'w')
 
     main_template.write(
         user_template.read() + base_template.read()
