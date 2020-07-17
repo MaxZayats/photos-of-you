46eae3580a7cd28b6068d721ba333672f5b51090
Maxim <49313691+MaxZayats@users.noreply.github.com>
2020-07-17 15:17:15 +0300

    Update modify_template.py

--- modules/modify_template.py
+++ modules/modify_template.py
@@ -4,9 +4,9 @@ def modify(user_template_name, base_template_name, main_template_name):
     В main_template_name добавляется скрипт и пользовательский шаблон.
 
     """
-    user_template = open('server/templates/'+user_template_name, 'r')
-    base_template = open('server/templates/'+base_template_name, 'r')
-    main_template = open('server/templates/'+main_template_name, 'w')
+    user_template = open('templates/'+user_template_name, 'r')
+    base_template = open('templates/'+base_template_name, 'r')
+    main_template = open('templates/'+main_template_name, 'w')
 
     main_template.write(
         user_template.read() + base_template.read()
