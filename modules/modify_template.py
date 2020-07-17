2e48becfda5ae315b8064520b071e04ae56fd32e
Maxim <49313691+MaxZayats@users.noreply.github.com>
2020-07-17 15:10:19 +0300

    Create modify_template.py

---
+++ modules/modify_template.py
@@ -0,0 +1,13 @@
+def modify(user_template_name, base_template_name, main_template_name):
+    """Фунция модифицирует шаблон main_template_name.
+
+    В main_template_name добавляется скрипт и пользовательский шаблон.
+
+    """
+    user_template = open('server/templates/'+user_template_name, 'r')
+    base_template = open('server/templates/'+base_template_name, 'r')
+    main_template = open('server/templates/'+main_template_name, 'w')
+
+    main_template.write(
+        user_template.read() + base_template.read()
+    )
